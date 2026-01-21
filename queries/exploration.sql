9768cf6c970f9a21b4b1a72b4169ddda483a4cb7
lorenzo-damicods <lorevoice04@gmail.com>
2026-01-21 02:32:36 +0100

    Add files via upload

--- queries/exploration.sql
+++ queries/exploration.sql
@@ -1,1 +1,6 @@
-.
+SELECT COUNT(*) FROM fraud.stg_transactions;
+SELECT COUNT(*) FROM fraud.fact_transactions;
+SELECT * FROM fraud.v_fraud_rate_by_geo LIMIT 10;
+SELECT * FROM fraud.v_fraud_rate_by_controls LIMIT 10;
+SELECT * FROM fraud.v_daily_monitoring LIMIT 10;
+SELECT * FROM fraud.v_expected_loss ORDER BY expected_loss_aed DESC LIMIT 20;
